26616b22cd5660071f2563429a8ae7af7ec56fdc
Samuel Fernández <samuel@digitalsynware.com>
2022-08-12 14:06:12 +0200

    fix (velocity): incorrect import

--- src/main/java/io/v4guard/plugin/velocity/listener/AntiVPNListener.java
+++ src/main/java/io/v4guard/plugin/velocity/listener/AntiVPNListener.java
@@ -3,11 +3,10 @@ package io.v4guard.plugin.velocity.listener;
 import com.velocitypowered.api.event.Continuation;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
 import io.v4guard.plugin.velocity.VelocityCheckProcessor;
 import io.v4guard.plugin.velocity.v4GuardVelocity;
-import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.event.EventHandler;
 
 public class AntiVPNListener {
 
@@ -22,9 +21,9 @@ public class AntiVPNListener {
         v4GuardVelocity.getCoreInstance().getCheckManager().runPreLoginCheck(e.getUsername(), e);
     }
 
-    @EventHandler(priority = Byte.MIN_VALUE)
+    @Subscribe(order = PostOrder.FIRST)
     public void onPostLogin(PostLoginEvent e) {
-        v4GuardVelocity.getCoreInstance().getCheckManager().runPostLoginCheck(e.getPlayer().getName(), e);
+        v4GuardVelocity.getCoreInstance().getCheckManager().runPostLoginCheck(e.getPlayer().getUsername(), e);
     }
 
 }
