02a537b19fd039fa61de66f3f7ed255c21a03df7
xIsm4 <minelatinsoporte@gmail.com>
2022-09-19 01:50:32 +0200

    feat(refactor); Avoid deprecation warning, clean code

--- src/main/java/io/v4guard/plugin/bungee/v4GuardBungee.java
+++ src/main/java/io/v4guard/plugin/bungee/v4GuardBungee.java
@@ -4,6 +4,7 @@ import io.v4guard.plugin.bungee.listener.AntiVPNListener;
 import io.v4guard.plugin.bungee.messager.Messager;
 import io.v4guard.plugin.core.mode.v4GuardMode;
 import io.v4guard.plugin.core.v4GuardCore;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.bstats.bungeecord.Metrics;
 
@@ -15,24 +16,36 @@ public class v4GuardBungee extends Plugin {
 
     @Override
     public void onEnable(){
-        this.getProxy().getConsole().sendMessage("§e[v4guard-plugin] (Bungee) Enabling...");
+        this.getProxy().getConsole().sendMessage(
+                new TextComponent("§e[v4guard-plugin] (Bungee) Enabling...")
+        );
         try {
             new Metrics(this, 16219);
-        } catch (Exception ex){
-            this.getProxy().getConsole().sendMessage("§e[v4guard-plugin] (Bungee) Failed to connect with bStats [WARN]");
+        }
+        catch (Exception ex){
+            this.getProxy().getConsole().sendMessage(
+                    new TextComponent("§e[v4guard-plugin] (Bungee) Failed to connect with bStats [WARN]")
+            );
         }
         try {
             core = new v4GuardCore(v4GuardMode.BUNGEE);
             core.getCheckManager().addProcessor(new BungeeCheckProcessor());
-        } catch (Exception e) {
-            this.getProxy().getConsole().sendMessage("§c[v4guard-plugin] (Bungee) Enabling... [ERROR]");
-            this.getProxy().getConsole().sendMessage("§cPlease check the console for more information and report this error.");
+        }
+        catch (Exception e) {
+            this.getProxy().getConsole().sendMessage(
+                    new TextComponent("§c[v4guard-plugin] (Bungee) Enabling... [ERROR]")
+            );
+            this.getProxy().getConsole().sendMessage(
+                    new TextComponent("§cPlease check the console for more information and report this error.")
+            );
             e.printStackTrace();
             return;
         }
         v4GuardBungee = this;
         this.getProxy().getPluginManager().registerListener(this, new AntiVPNListener());
-        this.getProxy().getConsole().sendMessage("§e[v4guard-plugin] (Bungee) Enabling... [DONE]");
+        this.getProxy().getConsole().sendMessage(
+                new TextComponent("§e[v4guard-plugin] (Bungee) Enabling... [DONE]")
+        );
         this.messager = new Messager();
     }
 
