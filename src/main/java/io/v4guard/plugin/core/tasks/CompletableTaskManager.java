35569b3f79adbbbaf9313cdfcef6b5d5e9e59530
Samuel Fernández <samuel@digitalsynware.com>
2022-11-24 13:22:43 +0100

    addition: mc brands can now be collected from the client (with option to disable it)

--- src/main/java/io/v4guard/plugin/core/tasks/CompletableTaskManager.java
+++ src/main/java/io/v4guard/plugin/core/tasks/CompletableTaskManager.java
@@ -1,6 +1,7 @@
 package io.v4guard.plugin.core.tasks;
 
 import io.v4guard.plugin.core.tasks.common.CompletableTask;
+import io.v4guard.plugin.core.tasks.types.CompletableMCBrandTask;
 
 import java.util.HashMap;
 
@@ -10,6 +11,18 @@ public class CompletableTaskManager {
 
     public HashMap<String, CompletableTask> getTasks() {
         return this.tasks;
+    }
+
+    public CompletableMCBrandTask getBrandTask(String username){
+        for(CompletableTask task : this.tasks.values()){
+            if(task instanceof CompletableMCBrandTask){
+                CompletableMCBrandTask brandTask = (CompletableMCBrandTask) task;
+                if(brandTask.getUsername().equals(username)){
+                    return brandTask;
+                }
+            }
+        }
+        return null;
     }
 
 }
--- src/main/java/io/v4guard/plugin/velocity/listener/PluginMessagingListener.java
+++ src/main/java/io/v4guard/plugin/velocity/listener/PluginMessagingListener.java
@@ -4,6 +4,11 @@ import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.proxy.Player;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.v4guard.plugin.bungee.v4GuardBungee;
+import io.v4guard.plugin.core.tasks.types.CompletableMCBrandTask;
+import io.v4guard.plugin.core.utils.StringUtils;
 import io.v4guard.plugin.velocity.v4GuardVelocity;
 import net.md_5.bungee.api.plugin.Listener;
 import org.bson.Document;
@@ -18,18 +23,26 @@ public class PluginMessagingListener implements Listener {
             if(e.getSource() instanceof Player){
                 if(e.getIdentifier().getId().equals("MC|Brand") || e.getIdentifier().getId().equals("minecraft:brand")){
                     Player player = (Player) e.getSource();
-                    Document data = new Document();
-                    data.put("username", player.getUsername());
-                    data.put("brand", new String(e.getData()));
-                    v4GuardVelocity.getCoreInstance().getBackendConnector().getSocket().emit("mc:brand", data.toJson());
+                    CompletableMCBrandTask task = v4GuardBungee.getCoreInstance().getCompletableTaskManager().getBrandTask(player.getUsername());
+                    if(task == null) task = new CompletableMCBrandTask(player.getUsername());
+                    task.addData(new String(e.getData()));
                 } else if (e.getIdentifier().getId().equals("LMC") || e.getIdentifier().getId().equals("labymod3:main")){
                     Player player = (Player) e.getSource();
-                    Document data = new Document();
-                    data.put("username", player.getUsername());
-                    data.put("brand", "labymod");
-                    v4GuardVelocity.getCoreInstance().getBackendConnector().getSocket().emit("mc:brand", data.toJson());
+                    CompletableMCBrandTask task = v4GuardBungee.getCoreInstance().getCompletableTaskManager().getBrandTask(player.getUsername());
+                    if(task == null) task = new CompletableMCBrandTask(player.getUsername());
+                    ByteBuf buf = Unpooled.wrappedBuffer(e.getData());
+                    String key = StringUtils.readString(buf, Short.MAX_VALUE);
+                    if(!key.equals("INFO")){
+                        return;
+                    }
+                    String json = StringUtils.readString(buf, Short.MAX_VALUE);
+                    Document data = Document.parse(json);
+                    String version = data == null ? "unknown" : (String) data.getOrDefault("version", "unknown");
+                    task.addData("labymod:" + version);
                 }
             }
         }
     }
+
+
 }
