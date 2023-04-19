4b9d55c788cbfed4c62d640bf48ac7d3874c3361
Alejandro G <58375195+zHackeed@users.noreply.github.com>
2023-04-19 20:37:43 +0200

    fix(bungee): ignore plugin messages that arent from mc brand or labbymod

--- src/main/java/io/v4guard/plugin/bungee/listener/PluginMessagingListener.java
+++ src/main/java/io/v4guard/plugin/bungee/listener/PluginMessagingListener.java
@@ -38,30 +38,32 @@ public class PluginMessagingListener implements Listener {
         ProxiedPlayer player = (ProxiedPlayer) e.getSender();
         if (v4GuardBungee.getCoreInstance().getBrandCheckManager().isPlayerAlreadyChecked(player.getUniqueId()) && !TEMP_CACHE.asMap().containsKey(player.getUniqueId())) return;
 
-        Document privacySettings = (Document) v4GuardBungee.getCoreInstance().getBackendConnector().getSettings().getOrDefault("privacy", new Document());
-        boolean invalidatedCache = (boolean) v4GuardBungee.getCoreInstance().getBackendConnector().getSettings().getOrDefault("invalidateCache", false);
+        if (e.getTag().equals("MC|Brand") || e.getTag().equals("minecraft:brand") || e.getTag().equals("LMC") || e.getTag().equals("labymod3:main")) {
+            Document privacySettings = (Document) v4GuardBungee.getCoreInstance().getBackendConnector().getSettings().getOrDefault("privacy", new Document());
+            boolean invalidatedCache = (boolean) v4GuardBungee.getCoreInstance().getBackendConnector().getSettings().getOrDefault("invalidateCache", false);
 
-        if (invalidatedCache && !privacySettings.getBoolean("collectMCBrand", true)) return;
+            if (invalidatedCache && !privacySettings.getBoolean("collectMCBrand", true)) return;
 
-        CompletableMCBrandTask task = v4GuardBungee.getCoreInstance().getCompletableTaskManager().getBrandTask(player.getName());
-        if (task == null) task = new CompletableMCBrandTask(player.getName());
+            CompletableMCBrandTask task = v4GuardBungee.getCoreInstance().getCompletableTaskManager().getBrandTask(player.getName());
+            if (task == null) task = new CompletableMCBrandTask(player.getName());
 
-        if (e.getTag().equals("MC|Brand") || e.getTag().equals("minecraft:brand")) {
-            task.addData(new String(e.getData()));
-        } else if (e.getTag().equals("LMC") || e.getTag().equals("labymod3:main")) {
-            ByteBuf buf = Unpooled.wrappedBuffer(e.getData());
-            String key = StringUtils.readString(buf, Short.MAX_VALUE);
-            if (!key.equals("INFO")) {
-                return;
+            if (e.getTag().equals("LMC") || e.getTag().equals("labymod3:main")) {
+                ByteBuf buf = Unpooled.wrappedBuffer(e.getData());
+                String key = StringUtils.readString(buf, Short.MAX_VALUE);
+                if (!key.equals("INFO")) {
+                    return;
+                }
+                String json = StringUtils.readString(buf, Short.MAX_VALUE);
+                Document data = Document.parse(json);
+                String version = data == null ? "unknown" : (String) data.getOrDefault("version", "unknown");
+
+                task.addData("labymod:" + version);
+            } else {
+                task.addData(new String(e.getData()));
             }
-            String json = StringUtils.readString(buf, Short.MAX_VALUE);
-            Document data = Document.parse(json);
-            String version = data == null ? "unknown" : (String) data.getOrDefault("version", "unknown");
 
-            task.addData("labymod:" + version);
+            TEMP_CACHE.put(player.getUniqueId(), System.currentTimeMillis());
+            v4GuardBungee.getCoreInstance().getBrandCheckManager().addPlayer(player.getUniqueId());
         }
-
-        TEMP_CACHE.put(player.getUniqueId(), System.currentTimeMillis());
-        v4GuardBungee.getCoreInstance().getBrandCheckManager().addPlayer(player.getUniqueId());
     }
 }
