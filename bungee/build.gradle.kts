b345e877f59397f1a988692daf701012575e0e5c
Alejandro G <contacto@zhacked.me>
2024-06-28 02:39:11 +0200

    feat(bungee): add bungee yml creator gradle plugin

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("connector.common-conventions")
     id("connector.shadow-conventions")
+    id("net.minecrell.plugin-yml.bungee") version "0.6.0"
 }
 
 dependencies {
@@ -15,4 +16,11 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+bungee {
+    name = "v4guard-plugin"
+    main = "io.v4guard.connector.platform.bungee.BungeeInstance"
+    version = project.version.toString()
+    description = "v4Guard connector for BungeeCord platform"
 }
