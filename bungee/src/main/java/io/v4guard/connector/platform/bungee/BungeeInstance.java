fb0b19b7711c896c3d64c37720580aa32c7316c2
Alejandro G <contacto@zhacked.me>
2025-01-21 15:20:12 +0100

    fix(bungeecord): handle correctly the user kick

--- bungee/src/main/java/io/v4guard/connector/platform/bungee/BungeeInstance.java
+++ bungee/src/main/java/io/v4guard/connector/platform/bungee/BungeeInstance.java
@@ -3,6 +3,7 @@ package io.v4guard.connector.platform.bungee;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import io.v4guard.connector.common.CoreInstance;
+import io.v4guard.connector.common.UnifiedLogger;
 import io.v4guard.connector.common.check.brand.BrandCheckProcessor;
 import io.v4guard.connector.common.check.settings.PlayerSettingsCheckProcessor;
 import io.v4guard.connector.common.compatibility.PlayerFetchResult;
@@ -17,13 +18,11 @@ import io.v4guard.connector.platform.bungee.listener.PlayerListener;
 import io.v4guard.connector.platform.bungee.listener.PlayerSettingsListener;
 import io.v4guard.connector.platform.bungee.listener.PluginMessagingListener;
 import io.v4guard.connector.platform.bungee.task.AwaitingKickTask;
-import net.md_5.bungee.UserConnection;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.protocol.Protocol;
 import org.bstats.bungeecord.Metrics;
 
 import java.util.concurrent.TimeUnit;
@@ -37,7 +36,7 @@ public class BungeeInstance extends Plugin implements UniversalPlugin {
     private BungeeCheckProcessor checkProcessor;
     private PluginMessagingListener brandCheckProcessor;
     private PlayerSettingsListener playerSettingsProcessor;
-    private Cache<String, AwaitingKick<ProxiedPlayer>> awaitedKickTaskCache;
+    private Cache<String, AwaitingKick<String>> awaitedKickTaskCache;
     private AwaitingKickTask awaitedKickTask;
     private CoreInstance coreInstance;
 
@@ -73,7 +72,7 @@ public class BungeeInstance extends Plugin implements UniversalPlugin {
                 .build();
 
 
-        this.awaitedKickTask = new AwaitingKickTask(this.awaitedKickTaskCache);
+        this.awaitedKickTask = new AwaitingKickTask(this.awaitedKickTaskCache, this.getProxy());
         this.schedule(this.awaitedKickTask, 0, 150, TimeUnit.MILLISECONDS);
 
         //this.getProxy().registerChannel(MessageReceiver.CHANNEL);
@@ -136,12 +135,12 @@ public class BungeeInstance extends Plugin implements UniversalPlugin {
     public void kickPlayer(String playerName, String reason, boolean later) {
         PlayerFetchResult<ProxiedPlayer> fetchedPlayer = fetchPlayer(playerName);
 
-        if (fetchedPlayer.isOnline()) {
-            if (later) {
-                awaitedKickTaskCache.put(playerName, new AwaitingKick<>(fetchedPlayer.getPlayer(), reason));
-                return;
-            }
+        if (later) {
+            awaitedKickTaskCache.put(playerName, new AwaitingKick<>(playerName, reason));
+            return;
+        }
 
+        if (fetchedPlayer.isOnline()) {
             fetchedPlayer.getPlayer().disconnect(TextComponent.fromLegacy(reason));
         }
     }
--- bungee/src/main/java/io/v4guard/connector/platform/bungee/task/AwaitingKickTask.java
+++ bungee/src/main/java/io/v4guard/connector/platform/bungee/task/AwaitingKickTask.java
@@ -3,27 +3,28 @@ package io.v4guard.connector.platform.bungee.task;
 import com.github.benmanes.caffeine.cache.Cache;
 import io.v4guard.connector.common.compatibility.kick.AwaitingKick;
 import net.md_5.bungee.UserConnection;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.protocol.Protocol;
 
 public class AwaitingKickTask implements Runnable {
 
-    private final Cache<String, AwaitingKick<ProxiedPlayer>> awaitedKickTaskCache;
+    private final Cache<String, AwaitingKick<String>> awaitedKickTaskCache;
+    private final ProxyServer proxyServer;
 
-    public AwaitingKickTask(Cache<String, AwaitingKick<ProxiedPlayer>> awaitedKickTaskCache) {
+    public AwaitingKickTask(Cache<String, AwaitingKick<String>> awaitedKickTaskCache, ProxyServer proxyServer) {
         this.awaitedKickTaskCache = awaitedKickTaskCache;
+        this.proxyServer = proxyServer;
     }
 
     @Override
     public void run() {
         awaitedKickTaskCache.asMap().forEach((playerName, kick) -> {
-            ProxiedPlayer player = kick.getPlayer();
+            ProxiedPlayer player = proxyServer.getPlayer(playerName);
+            if (player == null) return;
 
-            if (!(player instanceof UserConnection userConnection)) {
-                return;
-            }
-
+            if (!(player instanceof UserConnection userConnection)) return;
             if (userConnection.getCh().getEncodeProtocol() != Protocol.GAME) return;
 
             player.disconnect(TextComponent.fromLegacy(kick.getReason()));
