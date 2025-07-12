b62758342ad721a49f0bb1ce47357e9aadc5fa62
Alejandro G <contacto@zhacked.me>
2025-07-12 13:41:23 +0200

    feat(api): udpated settings listener (backwards comp)

--- api/src/main/java/io/v4guard/connector/api/constants/ListenersConstants.java
+++ api/src/main/java/io/v4guard/connector/api/constants/ListenersConstants.java
@@ -7,18 +7,18 @@ public class ListenersConstants {
     public static final String EVENT_CONNECT_ERROR = "connect_error";
     public static final String EVENT_CHECK = "check";
     public static final String EVENT_AUTH = "auth";
-    public static final String EVENT_SETTINGS = "settings2";
-    public static final String EVENT_SETTING = "setting";
+    public static final String EVENT_SETTINGS = "settings:load";
+    public static final String EVENT_SETTING = "settings:set";
     public static final String EVENT_CONSOLE = "console";
-    public static final String EVENT_MESSAGE  = "message";
-    public static final String EVENT_KICK  = "kick";
-    public static final String EVENT_CLEAN_CACHE  = "cleancache";
-    public static final String EVENT_FIND  = "find";
-    public static final String EVENT_RECONNECT  = "reconnect";
+    public static final String EVENT_MESSAGE = "message";
+    public static final String EVENT_KICK = "kick";
+    public static final String EVENT_CLEAN_CACHE = "cleancache";
+    public static final String EVENT_FIND = "find";
+    public static final String EVENT_RECONNECT = "reconnect";
 
     // Auth
-    public static final String AUTH_CODE  = "code";
-    public static final String AUTH_STATUS  = "status";
+    public static final String AUTH_CODE = "code";
+    public static final String AUTH_STATUS = "status";
 
     // Messenger
     public static final String ALL_PLAYERS_PERMISSION = "*";
