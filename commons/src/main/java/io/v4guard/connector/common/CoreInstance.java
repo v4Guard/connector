f29c293a973f074cd5bd69667e5557b9de0de1fb
Alejandro G <contacto@zhacked.me>
2026-09-19 18:26:25 +0200

    chore: bump version

--- commons/src/main/java/io/v4guard/connector/common/CoreInstance.java
+++ commons/src/main/java/io/v4guard/connector/common/CoreInstance.java
@@ -22,7 +22,7 @@ import java.util.logging.Logger;
 
 public class CoreInstance {
 
-    public static final String PLUGIN_VERSION = "3.1.1";
+    public static final String PLUGIN_VERSION = "3.2.0";
 
     private static CoreInstance instance;
     private PendingTasks pendingTasks;
